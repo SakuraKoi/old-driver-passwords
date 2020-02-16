ff2dbf7581c5ae07ac36d84c4188588327affb37
DriversDictionary <XXX@XXX.com>
2020-02-16 17:48:24 +0800

    call 7z to extract archive

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from argparse import ArgumentParser
 from pathlib import Path
+from subprocess import PIPE
+from subprocess import DEVNULL
+from subprocess import run
+from sys import exit
 
 
 def cli_main():
@@ -19,5 +23,39 @@ def cli_sort(dictionary: str = "resource/dictionary.txt"):
     print("end: sort")
 
 
+def cli_test(compressed: str, dictionary: str = "resource/dictionary.txt"):
+    """测试解压密码"""
+    # 测试 7z 是否存在
+    try:
+        exe7z = run(["7z", "-version"], stdout=PIPE,
+                    stderr=PIPE, encoding="utf-8")
+    except FileNotFoundError:
+        print("7z 不存在，请安装： https://www.7-zip.org/")
+        exit(-1)
+
+    with open(dictionary, "rt", encoding="utf-8") as _dict:
+        key = (i.rstrip("\n")
+               for i in iter(lambda: _dict.readline(), ""))
+        for n, i in enumerate(key):
+            try:
+                result = run(["7z", "x", "-y", "-oout", "-p{}".format(i),
+                              compressed], stdout=DEVNULL, stderr=PIPE, encoding="utf-8")
+                print("{}: #{}#".format(n, i))
+                if result.returncode == 0:
+                    print("findout: --- #{}# ---".format(i))
+                    break
+                else:
+                    if "Wrong password" in result.stderr:
+                        continue
+                    else:
+                        print("unknown error")
+                        break
+            except Exception as e:
+                print(e)
+                break
+        else:
+            print("no password matched.")
+
+
 if __name__ == "__main__":
-    cli_sort()
+    cli_test("README.zip")
